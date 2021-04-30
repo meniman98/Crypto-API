a0a2985a4120839695e36e3acbbd288f468bea48
Huncho Jack <meniman98@gmail.com>
2021-04-30 17:16:21 +0200

    get request works fine, sort works fine, database loads fine. what is left is the POST PUT and DELETE request

--- src/main/java/com/example/CoinAPI/LoadDatabase.java
+++ src/main/java/com/example/CoinAPI/LoadDatabase.java
@@ -15,9 +15,13 @@ public class LoadDatabase {
     CommandLineRunner initDatabase(CoinRepo repo) {
         return args -> {
             Coin bitcoin = new Coin.Builder("Bitcion").withTicker("BTC").withNumOfCoins(16_770_000).withMarketCap(189_580_000_000L).build();
-            System.out.println("Loading " + repo.save(new Coin.Builder("Bitcoin").build()));
-            System.out.println("Loading " + repo.save(new Coin.Builder("Some coin").build()));
-            System.out.println("Loading " + repo.save(new Coin.Builder("Another coin").build()));
+            Coin ethereum = new Coin.Builder("Ethereum").withTicker("ETH").withNumOfCoins(96_710_000).withMarketCap(69_280_000_000L).build();
+            Coin ripple = new Coin.Builder("Ripple").withTicker("XRP").withNumOfCoins(38_590_000).withMarketCap(64_750_000_000L).build();
+            Coin bitcoinCash = new Coin.Builder("BitcoinCash").withTicker("BCH").withNumOfCoins(16_670_000).withMarketCap(69_020_000_000L).build();
+            System.out.println("Loading " + repo.save(bitcoin));
+            System.out.println("Loading " + repo.save(ethereum));
+            System.out.println("Loading " + repo.save(ripple));
+            System.out.println("Loading " + repo.save(bitcoinCash));
         };
     }
 }
