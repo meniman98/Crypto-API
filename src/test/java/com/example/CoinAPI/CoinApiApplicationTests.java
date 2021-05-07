bc14b3e5d302d530e9c8c52251aeb2ac2d9ef30d
Huncho Jack <meniman98@gmail.com>
2021-05-07 03:21:35 +0200

    made the post test

--- src/test/java/com/example/CoinAPI/CoinApiApplicationTests.java
+++ src/test/java/com/example/CoinAPI/CoinApiApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.CoinAPI;
 
 import com.example.CoinAPI.controller.CoinController;
 import com.example.CoinAPI.model.Coin;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,6 +27,14 @@ class CoinApiApplicationTests {
 
     @Autowired
     private MockMvc mvc;
+
+    private String toJson(final Object object) {
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
+    }
 
     @Test
     void contextLoads() {
@@ -47,4 +57,14 @@ class CoinApiApplicationTests {
                 .andExpect(jsonPath("$.name", Matchers.is("Bitcoin")));
     }
 
+    // check if post works by creating a new Coin, then using the post request to see if a status 200 is given
+    @Test
+    void postCoin() throws Exception {
+        Coin coin = new Coin.Builder("sumCoin").build();
+
+        mvc.perform(post("/currencies")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(coin)))
+                .andExpect(status().is2xxSuccessful());
+    }
 }
